7e3164a5422f34f3ec800ec44ec279b02942ed4d
qinghe-xrx <ruixiangxie@outlook.com>
2024-10-18 12:02:14 +0800

    202410181202

--- Mooc_c/test_book/test02_4.c
+++ Mooc_c/test_book/test02_4.c
@@ -7,10 +7,10 @@ int main(void)
 {
 
     int n;
-    int flag = 1;
-    int denominator = 1;
+    int flag = -1;
+    int denom = 1;
     double tmp = 1;
-    double sum = 0;
+    double sum = -1;
 
     printf("Enter n:");
     scanf("%d", &n);
@@ -19,9 +19,15 @@ int main(void)
 
         for (int i = 1; i <= n; i++)
         {
-            
-
+            printf("sum = %.2f i = %d denom = %d\n", sum, i, denom);
+            tmp = flag * i * 1.0 / denom;
+            // printf("tmp = %d * %d / %d\n", flag, i, denom);
+            sum = sum + tmp;
+            printf("sum = %.2f + %.2f\n", sum, tmp);
+            flag = -flag;
+            denom = denom + 2;
         }
+        printf("\n");
         printf("sum=%f\n", sum);
     }
     else
